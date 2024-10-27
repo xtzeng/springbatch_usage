a6ac470be5a3bd7150f649e42a1161893ae2033e
nick <057342xxxsesef@gmail.com>
2024-10-27 19:09:43 +0800

    feat: springbatch_usage init commit

---
+++ src/test/java/com/flynne/springbatch/SpringbatchUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flynne.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchUsageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
